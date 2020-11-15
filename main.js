46975b612c6a342dfcc2efbde950284e0e4cc616
김태영 <rlaxodud9809@khu.ac.kr>
2020-11-16 03:43:31 +0900

    Add : add admin page (use ejs)

--- main.js
+++ main.js
@@ -6,12 +6,15 @@ var path = require('path');
 var mysql = require('./lib/db');
 var template = require('./lib/map_making_template');
 var bodyParser = require('body-parser');
+var ejs = require('ejs');
 var express = require('express') // express 모듈. npm install express를 통해 설치가능.
 
 '<script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=ec37ce7ff126878e77d2c814181f7794&libraries=services,clusterer,drawing"></script>';
 
-
 var app = express(); //express의 리턴값을 app에 담는다.
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -23,6 +26,7 @@ app.use(express.static('public'));
 //use를 사용해 매 접속마다 db에서 값을 갱신하도록 한다.
 app.use(function(req, res, next){
     //query문
+    //validity : (0: 표시x, 1: 표시o, 2: 추가요청, 3: 삭제요청)
   var sql = 'SELECT * FROM trash_addrs WHERE validity=1 or validity=3';
     //db에서 가져온 것을 담기위한 배열
   var temp_trash_data = new Array();
@@ -61,7 +65,7 @@ app.post('/add', function(req,res){
     trash_lat = req.body.lat;
     trash_lng = req.body.lng;
 
-    //validity : (0: 표시x, 1: 표시o, 2: 추가요청, 3: 수정요청)
+    //validity : (0: 표시x, 1: 표시o, 2: 추가요청, 3: 삭제요청)
     var sql = "INSERT INTO trash_addrs (gu_name, trash_addr, trash_lng, trash_lat, validity) VALUES(?,?,?,?,?)";
     mysql.query(sql, [gu_name, trash_addr, trash_lng, trash_lat, 2], function(err, result, field){
       if(err){
@@ -72,7 +76,7 @@ app.post('/add', function(req,res){
     })
 });
 
-app.post('/delete', function(req,res){
+app.post('/invisible_wait', function(req,res){
 
   trash_addr = req.body.addr;
   gu_name = req.body.gu_name;
@@ -88,6 +92,116 @@ app.post('/delete', function(req,res){
   });
 res.redirect('/');
 });
+
+app.post('/visible', function(req,res){
+  id = req.body.id;
+
+  var sql = "UPDATE trash_addrs SET validity=? WHERE id = ?";
+
+  mysql.query(sql, [1, id], function(err, result) {
+    console.log("Record Updated!!");
+    console.log(result);
+  });
+  res.redirect('/admin/list?category='+req.body.category+'&cur='+req.body.curPage);
+});
+
+app.post('/invisible', function(req,res){
+  id = req.body.id;
+
+  var sql = "UPDATE trash_addrs SET validity=? WHERE id = ?";
+
+  mysql.query(sql, [0, id], function(err, result) {
+    console.log("Record Updated!!");
+    console.log(result);
+  });
+  res.redirect('/admin/list?category='+req.body.category+'&cur='+req.body.curPage);
+});
+
+app.post('/delete', function(req,res){
+  id = req.body.id;
+
+  var sql = "DELETE FROM trash_addrs WHERE id = ?";
+  mysql.query(sql, [id], function(err, result) {
+    console.log("Record Updated!!");
+    console.log(result);
+  });
+
+res.redirect('/admin/list?category='+req.body.category+'&cur='+req.body.curPage);
+
+});
+
+app.get('/admin/gjfnlenvkajwgnkawjnvlawk', function(req,res){
+    
+});
+
+app.get("/admin/list/", function(req,res){
+  ///admin/list?category=int&cur=int
+  //한 페이지에 게시물 수
+  var page_size = 10;
+  //페이지의 갯수 : 1~10개 페이지
+  var page_list_size = 10;
+  //limit 변수
+  var no = "";
+  //전체 게시물의 숫자 (0으로 초기화)
+  var totalPageCount = 0;
+  //카테고리별 번호. (0: 표시x 처리, 1: 표시o 처리 , 2: 추가요청처리, 3: 삭제요청처리)(self)
+  var category = req.query.category;
+
+  var sql = 'SELECT count(*) as cnt FROM trash_addrs WHERE validity=?';
+
+  mysql.query(sql, [category], function(err, result) {
+    if(err){
+      console.log(err);
+      res.status(500).send("Internal Server Error");
+    }
+    //전체 게시물의 숫자.
+    totalPageCount = result[0].cnt;
+    //현재 페이지
+    var curPage = req.query.cur;
+
+    console.log("현재 페이지 : " + curPage, "전체 페이지 : " + totalPageCount);
+
+    var totalPage = Math.ceil(totalPageCount /page_size);//전체 페이지수 ceil함수로 정수로 변환
+    var totalSet = Math.ceil(totalPage/page_list_size);//전체 세트수(ex)1~10 1세트 1~15 2세트 1~33 : 4세트)
+    var curSet = Math.ceil(curPage/page_list_size); //현재 세트번호
+    var startPage = ((curSet-1)*10)+1; //현재 세트에서 출력된 시작 페이지 번호
+    var endPage = (startPage + page_list_size) -1 ; //현재 세트내 출력될 마지막 페이지 번호
+    
+    //현재페이지가 0보다 작으면
+    if(curPage<0){
+      no = 0;
+    }
+    else{
+      no = (curPage-1) *10; //db에서 가져올 첫번째 게시물 번호
+    }
+
+    var result2 = {
+      "category": category,
+      "curPage": curPage,
+      "page_list_size": page_list_size,
+      "page_size": page_size,
+      "totalPage": totalPage,
+      "totalSet": totalSet,
+      "curSet": curSet,
+      "startPage": startPage,
+      "endPage": endPage
+      };
+      
+    //나중에 db에 들어온 시간도 적어서 ORDER BY id해주자.
+    var sql = 'SELECT * FROM trash_addrs WHERE validity=? limit ?,?';
+    mysql.query(sql, [category, no, page_size], function(err, result) {
+      if(err){
+        console.log(err);
+        res.status(500).send("Internal Server Error");
+      }
+      console.log("몇번부터 몇번까지냐~~~~~~~" + no);
+
+      res.render('list',{data:result, paging:result2});
+    })
+  })
+})
+
+
 
 //서버가 비로소 여기에서 열린다. listen이 성공적으로 되면 function안에 있는 기능 수행.
 app.listen(3978, function() {
