47b87027d91e1dc1c33e360ba9f65db08fef10df
juicyorange <30497935+juicyorange@users.noreply.github.com>
2020-12-10 20:48:43 +0900

    Update get_geodata.py

--- python/get_geodata.py
+++ python/get_geodata.py
@@ -7,13 +7,13 @@ import pymysql.cursors #db연결
 
 # 카카오 developer에서 받은 REST API키를 입력하면 됩니다. 
 # 카카오map api에서 발급가능.
-rest_api_key = "66931185a093f52d46306e8486e8398c"
+rest_api_key = "키입력"
 
 # mysql ########################## 
 conn = pymysql.connect( 
-    host='localhost', 
+    host='', 
     user='root', 
-    password='Kim@1468433', 
+    password='', 
     db='trashmap', 
     charset='utf8' 
     )
